f5677c11c79c7ccad7224a62113591caf0707982
ilizin <ilio.zincone@gmx.com>
2025-03-30 12:03:35 +0200

    first commit

---
+++ src/test/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/ThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ilizin.spring_demo.springboot_demo.thymeleaf_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
