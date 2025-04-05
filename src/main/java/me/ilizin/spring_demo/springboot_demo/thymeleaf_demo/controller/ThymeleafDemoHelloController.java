603769766ab99391dde4b9e54b605f02b6b3f243
ilizin <ilio.zincone@gmx.com>
2025-04-05 20:48:46 +0200

    Added a different way to read a parameter from a form

--- src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/controller/ThymeleafDemoHelloController.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/controller/ThymeleafDemoHelloController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ThymeleafDemoHelloController {
@@ -21,6 +22,14 @@ public class ThymeleafDemoHelloController {
         String something = request.getParameter("somethingToSay");
         model.addAttribute("somethingToSayInUppercase", something.toUpperCase());
 
+        return "hello";
+    }
+
+    /* We're basically going to make use of RequestParam spring annotation, behind the scene spring will read
+       the parameter 'somethingToSay' from the request and bind it to the variable somethingToSay. */
+    @RequestMapping("/processForm2")
+    public String processForm2(@RequestParam("somethingToSay") String somethingToSay, Model model) {
+        model.addAttribute("somethingToSayInUppercase", somethingToSay.toUpperCase());
         return "hello";
     }
 }
