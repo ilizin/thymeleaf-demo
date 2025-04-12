5655177915753a595d0dc753c69ba705ba60ddd9
ilizin <ilio.zincone@gmx.com>
2025-04-12 16:01:06 +0200

    read the countries from a prperty file

--- src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/controller/ThymeleafDemoEmployeeController.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/controller/ThymeleafDemoEmployeeController.java
@@ -3,20 +3,27 @@ package me.ilizin.spring_demo.springboot_demo.thymeleaf_demo.controller;
 import me.ilizin.spring_demo.springboot_demo.thymeleaf_demo.model.Employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class ThymeleafDemoEmployeeController {
     private static final Logger logger = LoggerFactory.getLogger(ThymeleafDemoEmployeeController.class);
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showEmployeeForm")
     public String showEmployeeForm(Model model) {
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
+        model.addAttribute("countries", countries);
         return "employee-form";
     }
 
