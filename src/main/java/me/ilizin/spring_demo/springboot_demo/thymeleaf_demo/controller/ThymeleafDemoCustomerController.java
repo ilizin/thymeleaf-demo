c739e4cc4fe54f9e64712d782137a560cb458bad
ilizin <ilio.zincone@gmx.com>
2025-04-26 12:37:41 +0200

    Added Customer entity

---
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/controller/ThymeleafDemoCustomerController.java
@@ -0,0 +1,8 @@
+package me.ilizin.spring_demo.springboot_demo.thymeleaf_demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ThymeleafDemoCustomerController {
+
+}
---
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/model/Customer.java
@@ -0,0 +1,31 @@
+package me.ilizin.spring_demo.springboot_demo.thymeleaf_demo.model;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class Customer {
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
