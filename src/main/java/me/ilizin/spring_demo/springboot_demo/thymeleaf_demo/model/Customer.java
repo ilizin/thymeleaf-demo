8e3455d3a68a014f9eeab13ae26efa49df3c5d32
ilizin <ilio.zincone@gmx.com>
2025-05-10 15:43:22 +0200

    added regex validation

--- src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/model/Customer.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/model/Customer.java
@@ -1,9 +1,6 @@
 package me.ilizin.spring_demo.springboot_demo.thymeleaf_demo.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -18,6 +15,9 @@ public class Customer {
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to ten")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only five chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -41,5 +41,13 @@ public class Customer {
 
     public void setFreePasses(int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
