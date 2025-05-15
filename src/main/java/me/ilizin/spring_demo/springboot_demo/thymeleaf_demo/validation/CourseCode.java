e68b152155031f46e6827a21e3176566dbf12d4d
ilizin <ilio.zincone@gmx.com>
2025-05-15 21:56:55 +0200

    add validation logic

--- src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/validation/CourseCode.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/validation/CourseCode.java
@@ -1,6 +1,7 @@
 package me.ilizin.spring_demo.springboot_demo.thymeleaf_demo.validation;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,4 +12,14 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME) //Retain this annotation in the Java class file
 public @interface CourseCode {
+    public String value() default "LUV";
+
+    public String message() default "must start with LUV";
+
+    // Define default groups
+    public Class<?>[] groups() default {};
+
+    // Define default payloads
+    // Payloads: provide custom details about validation failure (severity level, error code, etc.)
+    public Class<? extends Payload>[] payload() default {};
 }
---
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/thymeleaf_demo/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,19 @@
+package me.ilizin.spring_demo.springboot_demo.thymeleaf_demo.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode courseCode) {
+        coursePrefix = courseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        return value.startsWith(coursePrefix);
+    }
+}
